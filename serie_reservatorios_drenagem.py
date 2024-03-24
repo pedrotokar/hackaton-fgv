3c369e62ebce229ae0256c7b56cdf3b5af0aa169
Larissa <larissahamlet@gmail.com>
2024-03-24 20:27:51 -0300

    mudei legenda

--- serie_reservatorios_drenagem.py
+++ serie_reservatorios_drenagem.py
@@ -6,10 +6,10 @@ df = pd.read_csv(r"C:\Users\laris\OneDrive\Documentos\hackaton-fgv\dados\drenage
 
 df['data_hora'] = pd.to_datetime(df['data_particao'] + ' ' + df['horario'])
 
-for i in range(3):
+for i in range(2):
     df.loc[df['altura_agua'] > 30, 'altura_agua'] /= 100
 
-df = df.drop(df[df['altura_agua'] > 3].index)
+df = df.drop(df[df['altura_agua'] > 30].index)
 df = df.sort_values(by="data_hora")
 
 fig = px.line(df, x='data_hora', y='altura_agua', color='nome_reservatorio')
@@ -17,40 +17,23 @@ fig = px.line(df, x='data_hora', y='altura_agua', color='nome_reservatorio')
 fig.update_layout(title='Altura da Água em Reservatórios de Drenagem',
     xaxis_title='Data',
     yaxis_title='Altura da Água (m)',
-    legend_title='Reservatório',
+    legend_title='Reservatório e Nível de Alerta',
     barmode='overlay',
     plot_bgcolor='white',
-    template='ygridoff'
+    template='ygridoff',
 )
 
 fig.update_xaxes(ticks='outside')
 
-fig.add_shape(type="line", x0=df['data_hora'].min(), x1=df['data_hora'].max(), y0=1, y1=1,
-              line=dict(color="lightskyblue", width=2))
-fig.add_shape(type="line", x0=df['data_hora'].min(), x1=df['data_hora'].max(), y0=1.5, y1=1.5,
-              line=dict(color="pink", width=2))
-fig.add_shape(type="line", x0=df['data_hora'].min(), x1=df['data_hora'].max(), y0=2.5, y1=2.5,
-              line=dict(color="black", width=2))
+fig.add_shape(type="line", showlegend=True, legendgroup="Níveis de Alerta", name="Atenção", x0=df['data_hora'].min(), x1=df['data_hora'].max(), y0=1, y1=1,
+              line=dict(color="lightskyblue", width=2, dash="dashdot",))
+fig.add_shape(type="line", showlegend=True, legendgroup="Níveis de Alerta", name="Alerta",x0=df['data_hora'].min(), x1=df['data_hora'].max(), y0=1.5, y1=1.5,
+              line=dict(color="yellow", width=2, dash="dashdot",))
+fig.add_shape(type="line", showlegend=True, legendgroup="Níveis de Alerta", name="Extravasamento", x0=df['data_hora'].min(), x1=df['data_hora'].max(), y0=2.5, y1=2.5,
+              line=dict(color="black", width=2, dash="dashdot",))
 
-# Adicionar rótulos
-fig.add_annotation(x=df['data_hora'].min(), y=1, text="Atenção", showarrow=False, yshift=13, xshift=31, font=dict(
-            size=10
-            ),
-        borderwidth=1,
-        bgcolor="white",
-        opacity=0.9)
-fig.add_annotation(x=df['data_hora'].min(), y=1.5, text="Alerta", showarrow=False, yshift=13, xshift=23, font=dict(
-            size=10
-            ),
-        borderwidth=1,
-        bgcolor="white",
-        opacity=0.9)
-fig.add_annotation(x=df['data_hora'].min(), y=2.5, text="Extravasamento", showarrow=False, yshift=13, xshift=49 , font=dict(
-            size=10
-            ),
-        borderwidth=1,
-        bgcolor="white",
-        opacity=0.9)
+
+
 
 # Exibindo os gráficos lado a lado
 fig.show()
