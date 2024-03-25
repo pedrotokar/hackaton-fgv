bccf13383acd5825d15a28d8eb73c2e9d9c14bb2
pedrotokar <pedrotokar2004@gmail.com>
2024-03-25 13:17:30 -0300

    Melhorei a página de chamados do rjcor

--- pages/ocorrencias_rjcor.py
+++ pages/ocorrencias_rjcor.py
@@ -5,81 +5,86 @@ import matplotlib as mpl
 import pydeck as pdk
 import json
 import datetime
+from core.scripts import get_data
 
 import streamlit as st
 
-query = """SELECT *
+
+def retorna_query(data):
+    return f"""SELECT *
     FROM datario.adm_cor_comando.ocorrencias
-    WHERE data_particao > DATE("2023-06-01")""" # Coloquei um limite pra não gastar 3GB do google quando for puxar
-chamados_rjcor = pandas_gbq.read_gbq(query, project_id = "hackaton-fgv")
-#chamados_rjcor.to_csv("rjcor.csv")
-#chamados_rjcor = pd.read_csv("rjcor.csv")
+    WHERE data_particao > DATE("{data}")""" # Coloquei um limite pra não gastar 3GB do google quando for puxar
 
-chamados_rjcor["data_inicio"] = pd.to_datetime(chamados_rjcor["data_inicio"])
-chamados_rjcor["data_inicio_string"] = chamados_rjcor.apply(lambda linha: linha["data_inicio"].strftime("%d/%m/%Y, %H:%M:%S"), axis=1)
+def retorna_dados_pela_data(data):
+    query = retorna_query(data)
+    chamados_rjcor = get_data(query, project_id = "hackaton-fgv")
 
-chamados_rjcor["gravidade"].value_counts(dropna = False)
+    chamados_rjcor["data_inicio"] = pd.to_datetime(chamados_rjcor["data_inicio"])
+    chamados_rjcor["data_inicio_string"] = chamados_rjcor.apply(lambda linha: linha["data_inicio"].strftime("%d/%m/%Y, %H:%M:%S"), axis=1)
 
+    gravidades = {
+        "Normal": [255, 255, 0],
+        "Média": [255, 200, 0],
+        "Alta": [255, 0, 0],
+        None: [200, 200, 200],
+        "nan": [200, 200, 200],
+        "Baixo": [255, 255, 0],
+        "Medio": [255, 200, 0],
+        "Alto": [255, 0, 0]
+    }
 
-gravidades = {
-    "Normal": [100, 255, 0],
-    "Média": [255, 200, 0],
-    "Alta": [255, 0, 0],
-    None: [200, 200, 200],
-    "nan": [200, 200, 200],
-    "Baixo": [255, 255, 0],
-    "Medio": [255, 200, 0],
-    "Alto": [255, 0, 0]
-}
-def aplica_cores(linha):
-    if linha["status"] == "Fechado":
-        return [0, 255, 0]
-    else:
-        return gravidades[linha["gravidade"]]
-chamados_rjcor["coloracao"] = chamados_rjcor.apply(aplica_cores, axis=1)
+    def aplica_cores(linha):
+        if linha["status"] == "Fechado":
+            return [0, 255, 0]
+        else:
+            return gravidades[linha["gravidade"]]
+    chamados_rjcor["coloracao"] = chamados_rjcor.apply(aplica_cores, axis=1)
 
+    return chamados_rjcor
 
+def monta_layer_abertos(dados):
+     return pdk.Layer(
+        "ScatterplotLayer",
+        dados[["latitude", "longitude", "coloracao", "bairro", "data_inicio_string",
+            "gravidade", "descricao", "status"]][dados["status"] == "Aberto"],
+        get_position = ["longitude", "latitude"],
+        auto_highlight = True,
+        get_radius = "200",
+        get_fill_color = "coloracao",
+        pickable = True
+    )
 
-camera_inicial = pdk.ViewState(
-    latitude = -22.9035, 
-    longitude = -43.4096, 
-    zoom = 9.3, 
-    max_zoom = 16, 
-    pitch = 0, 
-    bearing = 0
-)
+def monta_layer_fechados(dados):
+    return pdk.Layer(
+        "ScatterplotLayer",
+        dados[["latitude", "longitude", "coloracao", "bairro", "data_inicio_string",
+            "gravidade", "descricao", "status"]][dados["status"] == "Fechado"],
+        get_position = ["longitude", "latitude"],
+        auto_highlight = True,
+        get_radius = "100",
+        get_fill_color = "coloracao",
+        pickable = True
+    )
 
-base = pdk.Layer(
-    "PolygonLayer",
-    stroked = False,
-    get_polygon = "-",
-    get_fill_color = [0, 0, 0, 20],
-)
+def monta_mapa(layers):
+    camera_inicial = pdk.ViewState(
+        latitude = -22.9035,
+        longitude = -43.4096,
+        zoom = 9.3,
+        max_zoom = 16,
+        pitch = 0,
+        bearing = 0
+    )
 
-ocorrencias_acontecendo = pdk.Layer(
-    "ScatterplotLayer",
-    chamados_rjcor[["latitude", "longitude", "coloracao", "bairro", "data_inicio_string", 
-        "gravidade", "descricao", "status"]][chamados_rjcor["status"] == "Aberto"],
-    get_position = ["longitude", "latitude"],
-    auto_highlight = True,
-    get_radius = "200",
-    get_fill_color = "coloracao",
-    pickable = True
-)
+    base = pdk.Layer(
+        "PolygonLayer",
+        stroked = False,
+        get_polygon = "-",
+        get_fill_color = [0, 0, 0, 20],
+    )
 
-ocorrencias_fechadas = pdk.Layer(
-    "ScatterplotLayer",
-    chamados_rjcor[["latitude", "longitude", "coloracao", "bairro", "data_inicio_string", 
-        "gravidade", "descricao", "status"]][chamados_rjcor["status"] == "Fechado"],
-    get_position = ["longitude", "latitude"],
-    auto_highlight = True,
-    get_radius = "100",
-    get_fill_color = "coloracao",
-    pickable = True
-)
-
-r = pdk.Deck(
-    layers = [base, ocorrencias_acontecendo], 
+    return pdk.Deck(
+    layers = [base,*layers],
     initial_view_state = camera_inicial,
     tooltip = {"html": "<b>Bairro:</b> {bairro}<br><b>Data de inicio:</b> {data_inicio_string}"\
                "<br><b>Gravidade:</b> {gravidade}<br><b>Endereço:</b> {descricao}"\
@@ -92,4 +97,16 @@ r = pdk.Deck(
     map_style = "dark_no_labels"
 )
 
-st.pydeck_chart(r)
+dados = retorna_dados_pela_data("2023-06-01")
+st.write("Selecione quais tipos de chamado você quer visualizar.")
+chamados_abertos = st.checkbox("Chamados Abertos")
+chamados_fechados = st.checkbox("Chamados fechados")
+layer = []
+if chamados_abertos:
+    layer.append(monta_layer_abertos(dados))
+if chamados_fechados:
+    layer.append(monta_layer_fechados(dados))
+
+mapa = monta_mapa(layer)
+
+st.pydeck_chart(mapa)
