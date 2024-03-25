843ba6e6163c7809b7a135c91099fcbeb2c94820
pedrotokar <pedrotokar2004@gmail.com>
2024-03-25 13:37:49 -0300

    Melhorei a visualização dos pontos críticos

--- pages/pontos_criticos.py
+++ pages/pontos_criticos.py
@@ -6,35 +6,41 @@ import matplotlib as mpl
 import pydeck as pdk
 import json
 
+import streamlit as st
+
 query = """SELECT *
-    FROM rj-rioaguas.saneamento_drenagem.ponto_supervisionado_alagamento""" # Coloquei um limite pra não gastar 3GB do google quando for puxar
-drenagem = pandas_gbq.read_gbq(query, project_id = "hackaton-fgv")
+FROM rj-rioaguas.saneamento_drenagem.ponto_supervisionado_alagamento"""
 
-
-drenagem[drenagem["classe"] == "Ponto observado"]["eliminado"].value_counts()
-
+dados_drenagem = pandas_gbq.read_gbq(query, project_id = "hackaton-fgv")
+dados_drenagem[dados_drenagem["classe"] == "Ponto observado"]["eliminado"].value_counts()
 
 classes = {
     "Ponto critico": [0, 0, 190],
     "Ponto monitorado": [0, 255, 0],
     "Ponto observado": [100, 100, 255]
 }
-drenagem["coloracao"] = drenagem.apply(lambda linha: classes[linha["classe"]], axis=1)
-
+dados_drenagem["coloracao"] = dados_drenagem.apply(lambda linha: classes[linha["classe"]], axis=1)
 
 def arruma_descricao(linha):
     if linha["causa_alagamento"] == " " or linha["causa_alagamento"] is None:
         return "Não foi fornecida descrição para esse ponto"
     else:
         return linha["causa_alagamento"]
-drenagem["descricao"] = drenagem.apply(arruma_descricao, axis=1)
+dados_drenagem["descricao"] = dados_drenagem.apply(arruma_descricao, axis=1)
 
 def arruma_medida(linha):
     if linha["medida_cor"] == "_" or linha["medida_cor"] is None:
         return "Não foi fornecida medida planejada ou tomada para esse ponto"
     else:
         return linha["medida_cor"]
-drenagem["medida"] = drenagem.apply(arruma_medida, axis=1)
+dados_drenagem["medida"] = dados_drenagem.apply(arruma_medida, axis=1)
+
+def arruma_resolvido(linha):
+    if linha["eliminado"] == " " or linha["eliminado"] is None:
+        return "Ainda não foi eliminado"
+    else:
+        return linha["eliminado"]
+dados_drenagem["eliminado"] = dados_drenagem.apply(arruma_resolvido, axis=1)
 
 camera_inicial = pdk.ViewState(
     latitude = -22.9035, 
@@ -54,8 +60,8 @@ base = pdk.Layer(
 
 drenagens_nao_resolvidas = pdk.Layer(
     "ScatterplotLayer",
-    drenagem[["latitude", "longitude", "coloracao", "endereco_ponto_supervisionado", "bairro", 
-        "sub_bacia_hidrografica", "medida", "descricao", "eliminado"]][drenagem["classe"] == "Ponto critico" | drenagem["classe"] == "Ponto observado"],
+    dados_drenagem[["latitude", "longitude", "coloracao", "endereco_ponto_supervisionado", "bairro",
+        "sub_bacia_hidrografica", "medida", "descricao", "eliminado"]][(dados_drenagem["classe"] == "Ponto critico") | (dados_drenagem["classe"] == "Ponto observado")],
     get_position = ["longitude", "latitude"],
     auto_highlight = True,
     get_radius = 200,
@@ -65,8 +71,8 @@ drenagens_nao_resolvidas = pdk.Layer(
 
 drenagens_resolvidas = pdk.Layer(
     "ScatterplotLayer",
-    drenagem[["latitude", "longitude", "coloracao", "endereco_ponto_supervisionado", "bairro", 
-        "sub_bacia_hidrografica", "medida", "descricao", "eliminado"]][drenagem["classe"] == "Ponto monitorado"],
+    dados_drenagem[["latitude", "longitude", "coloracao", "endereco_ponto_supervisionado", "bairro",
+        "sub_bacia_hidrografica", "medida", "descricao", "eliminado"]][dados_drenagem["classe"] == "Ponto monitorado"],
     get_position = ["longitude", "latitude"],
     auto_highlight = True,
     get_radius = 200,
@@ -74,9 +80,17 @@ drenagens_resolvidas = pdk.Layer(
     pickable = True
 )
 
+st.write("Selecione quais tipos de pontos você quer analisar.")
+chamados_abertos = st.checkbox("Pontos observados e críticos (não foram resolvidos)")
+chamados_fechados = st.checkbox("Pontos monitorados (já foram resolvidos)")
+layer = []
+if chamados_abertos:
+    layer.append(drenagens_nao_resolvidas)
+if chamados_fechados:
+    layer.append(drenagens_resolvidas)
 
 mapa = pdk.Deck(
-    layers = [base, drenagens], 
+    layers = [base, *layer],
     initial_view_state = camera_inicial,
     tooltip = {"html": "<b>Endereço:</b> {endereco_ponto_supervisionado} - {bairro}"\
                "<br><b>Sub bacia hidrografica:</b> {sub_bacia_hidrografica}<br>"\
@@ -87,7 +101,7 @@ mapa = pdk.Deck(
                   "color": "white"
                         }
               },
-    map_style = "dark_no_labels"
+    map_style = "light_no_labels"
 )
 
-mapa.to_html("geojson_layer.html")
+st.pydeck_chart(mapa)
